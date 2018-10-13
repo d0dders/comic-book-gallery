533ef90c0ba87132ade1a6af2d2a813a79fd6073
d0dders <dodders@outlook.com>
2018-10-13 22:28:58 +0100

    Added series repository

---
+++ src/ComicBookGallery/Data/SeriesRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int id)
+        {
+            Series seriesToReturn = null;
+            foreach (var series in Data.Series)
+            {
+
+                if (series.Id == id)
+                {
+                    seriesToReturn = series;
+                    break;
+                }
+            }
+            if (seriesToReturn != null)
+            {
+                //TODO Get issues for this series
+                var comicBooks = new ComicBook[0];
+                foreach (var comicBook in Data.ComicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.Id == id)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+                }
+                seriesToReturn.Issues = comicBooks;
+            }
+            return seriesToReturn;
+        }
+    }
+}
